9e086f28f40762cf740e30ccff87c1ea70e50bee
Ian Wild <i.wild@ulcc.ac.uk>
2012-12-07 17:07:19 +0000

    'ual_db_process'-ing will need to be initialed using cron.

---
+++ cli/sync.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * CLI sync for full UALMIS synchronisation for UAL.
+ *
+ * Sample cron entry:
+ * # 5 minutes past 4am
+ * 5 4 * * * $sudo -u www-data /usr/bin/php /var/www/moodle/local/ual_db_process/cli/sync.php
+ *
+ * Notes:
+ *   - it is required to use the web server account when executing PHP CLI scripts
+ *   - you need to change the "www-data" to match the apache user account
+ *   - use "su" if "sudo" not available
+ *
+ * @package    local
+ * @subpackage ual_db_process
+ * @copyright  2012 University of London Computer Centre
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(dirname(__FILE__).'/../lib.php');
+
+$mis = new target_mis();
+
+$mis->perform_sync();
+
+
